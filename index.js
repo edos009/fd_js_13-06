d07d1a6d64d5a88f6a9ae97cd9265f4ae57da9f4
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-14 14:47:32 +0300

    Example get this different ways

--- index.js
+++ index.js
@@ -1,122 +1,21 @@
-/**
- *
- * @param {string} fname
- * @param {string} lname
- * @param {number} age
- * @param {boolean} isMale
- * @param {string} email
- * @param {boolean} [isSubscribed=false]
- */
-function User(fname, lname, age, isMale, email, isSubscribed = false) {
-  this.fname = fname;
-  this.lname = lname;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribed = isSubscribed;
-}
+"use strict";
 
-function UserPrototype() {
-  this.fullName = function () {
-    return `${this.fname} ${this.lname}`;
-  };
-}
+const site = {
+  title: "green site",
+  headers: ["header 1", "header 2", "header 3"],
+  showHeaders() {
+    console.log(this);
+    this.headers.forEach((header) => {
+      console.log(`${header} | ${this.title}`);
+    });
+  },
+};
 
-User.prototype = new UserPrototype();
+site.showHeaders();
 
-function createUsers(amount = 1) {
-  const arrUsers = [];
-
-  for (let i = 0; i < amount; i++) {
-    const user = new User(
-      `Eduard${i}`,
-      `Tiutiunnyk${i}`,
-      Math.ceil(Math.random() * (60 - 20) + 20),
-      Math.random() > 0.5,
-      `email${i}@gmail.com`
-    );
-    arrUsers.push(user);
-  }
-
-  return arrUsers;
-}
-
-const users = createUsers(20);
-console.table(users);
-
-//Массив пользователей с полными именами
-
-const arrFullName = users.map(function (user) {
-  return user.fullName();
-});
-console.table(arrFullName);
-
-//Массив пользователей, которые старше 40
-
-const arrUsersOverForty = users.filter(function (user) {
-  return user.age > AGE_LIMIT;
-});
-
-console.table(arrUsersOverForty);
-
-//Модифицировать сабсрайп пользователей - рандомно
-
-users.forEach(function (user) {
-  user.isSubscribed = Math.random() > 0.5;
-});
-
-console.table(users);
-
-//Получить массив эмейлов пользователей до 40 лет женского пола с подпиской
-
-const womenOverFortyIsSubscribeEmail = users
-  .filter(function (user) {
-    return user.age > AGE_LIMIT;
-  })
-  .filter(function (user) {
-    return !user.isMale;
-  })
-  .filter(function (user) {
-    return user.isSubscribed;
-  })
-  .map(function (user) {
-    return user.email;
-  });
-
-console.table(womenOverFortyIsSubscribeEmail);
-
-// Получить массив полных имен пользователей мужчин которые младше 40 и без подписки
-
-const fullNameMenLeesFortyIsNotSubscribe = users
-  .filter(function (user) {
-    return user.age < AGE_LIMIT;
-  })
-  .filter(function (user) {
-    return user.isMale;
-  })
-  .filter(function (user) {
-    return !user.isSubscribed;
-  })
-  .map(function (user) {
-    return user.fullName();
-  })
-
-console.table(fullNameMenLeesFortyIsNotSubscribe);
-
-// Получить сумму возрастов
-
-const sumAgeMenLeesFortyIsNotSubscribe = users
-  .filter(function (user) {
-    return user.age < AGE_LIMIT;
-  })
-  .filter(function (user) {
-    return user.isMale;
-  })
-  .filter(function (user) {
-    return !user.isSubscribed;
-  })
-  .reduce(function (initialV, user) {
-    return initialV + user.age
-  }, 0);
-
-console.table(sumAgeMenLeesFortyIsNotSubscribe);
+/*
+1 - arrow function;
+2 - save this in variable - self;
+3 - argument for callback - this;
+4 - function.bind(this);
+*/
