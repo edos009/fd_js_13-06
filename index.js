2d139f85659443d6c469f70f8d863e49406b97ae
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-14 16:01:31 +0300

    Example with rest and spred operators

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 "use strict";
 
 //Get summa all arguments function
-const getSum = (...argument) => argument.reduce((accumulator, element) => accumulator + element);
+const getSum = (...theArguments) =>
+  theArguments.reduce((accumulator, element) => accumulator + element);
 
 console.log(getSum(1, 2, 3, 4, 5, 6));
+
+const arr1 = ['a', 'e', 'i', 'o', 'u', 'y'];
+const arr2 = ['k', 'l', 'm', 'n', 'b', 'x'];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
