601524ade5fd1657457d2b3c998e8160db93ac40
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-15 16:25:14 +0300

    Example toJadenCase

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 "use strict";
 
-//Рекурсия - это способность функции, которая вызывает сама себя.
+//
+const str = "to Be or NoT to bE";
 
-const fuctorial = (num = 4) => {
-  if (num < 0) {
-    return null;
-  }
-  if (num === 1 || num === 0) {
-    return 1;
-  }
-  return num * fuctorial(num - 1);
-};
+const getStringToJadenCase = (string, separator = " ") =>
+  string
+    .split(separator)
+    .map((elem) => elem.at(0).toUpperCase() + elem.toLowerCase().slice(1))
+    .join(" ");
 
-console.log(fuctorial(5));
-
-  debugger;
-const getPow = (num = 2, pow = 3) => {
-  if (pow < 0) {
-    return null;
-  }
-  if (pow === 0) {
-    return 1;
-  }
-  return num * getPow(num, pow - 1);
-};
-
-console.log(getPow());
+console.log(getStringToJadenCase(str));
