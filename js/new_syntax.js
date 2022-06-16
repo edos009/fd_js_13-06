1aaaaa063d8813218abdec531add9f98f5e556a9
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-16 17:22:50 +0300

    Example with Auto and Fuel - Class, Getter, Setter

--- js/new_syntax.js
+++ js/new_syntax.js
@@ -1,7 +1,7 @@
 //! Пример записи через класс
 
 "use strict";
-
+/*
 class Woker {
   constructor(fname, sname, rate, days) {
     this.fname = fname;
@@ -32,6 +32,7 @@ class Woker {
 const woker = new Woker("Eduard", "Tiutiunnyk", 1000, 22);
 
 console.log(woker.getSalary());
+*/
 
 /*
 class UserClass {
@@ -65,3 +66,91 @@ const userNew = new UserClass("Eduard", "Tiutiunnyk", 26);
 
 console.log(userNew.isAdult());
 */
+
+class Auto {
+  #model;
+  constructor(model, ownWeight) {
+    this.#model = model;
+    this.ownWeight = ownWeight;
+  }
+
+  get ownWeight() {
+    return this._ownWeight;
+  }
+
+  set ownWeight(ownWeight) {
+    if (typeof ownWeight !== "number") {
+      throw new TypeError("Own weight must be number.");
+    }
+
+    if (ownWeight < OWN_WEIGHT_MIN || ownWeight > OWN_WEIGHT_MAX) {
+      throw new RangeError(
+        "Own weight must be " + OWN_WEIGHT_MIN + ".." + OWN_WEIGHT_MAX
+      );
+    }
+
+    this._ownWeight = ownWeight;
+  }
+
+  getFullWeight(objFuel) {
+    if (Fuel.isFuel(objFuel) === false) {
+      throw new TypeError("Object fuel must be object");
+    }
+    return this._ownWeight + objFuel.weight;
+  }
+}
+
+const auto = new Auto('bmw', 1700)
+
+class Fuel {
+  constructor(volume, density) {
+    this.volume = volume;
+    this.density = density;
+  }
+
+  get volume() {
+    return this._volume;
+  }
+
+  set volume(volume) {
+    if (typeof volume !== "number") {
+      throw new TypeError("Volume must be number.");
+    }
+
+    if (volume < 0 || volume > VOLUME_MAX) {
+      throw new RangeError("Volume must be 0.." + VOLUME_MAX);
+    }
+
+    this._volume = volume;
+  }
+
+  get density() {
+    return this._density;
+  }
+
+  set density(density) {
+    if (typeof density !== "number") {
+      throw new TypeError("Density must be number.");
+    }
+
+    if (density < DENSITY_MIN || density > DENSITY_MAX) {
+      throw new RangeError(
+        "Volume must be " + DENSITY_MIN + ".." + DENSITY_MAX
+      );
+    }
+
+    this._density = density;
+  }
+
+  get weight() {
+    return this._volume * this._density;
+  }
+
+  static isFuel(obj) {
+    return obj instanceof Fuel;
+  }
+}
+
+const fuel = new Fuel(30, 0.7)
+
+console.log(auto.getFullWeight(fuel));
