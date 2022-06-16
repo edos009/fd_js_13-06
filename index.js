c5ebc5111c73d2db77d1af2cc21476ecfe38e508
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-16 14:14:53 +0300

    Add new structure

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
 "use strict";
 
-//
-const str = "I love to eat ice cream";
-const vowels = ["a", "e", "i", "o", "u", "y"];
 
-const getCountVowel = (string, vowels) =>
-  string
-    .toLowerCase()
-    .split("")
-    .filter((elem) => vowels.includes(elem)).length;
-
-console.log(getCountVowel(str, vowels));
-
-//"Fistname Lastname" => "Lastname F."
-
-const fullName = "edik tiutiunnyk";
-
-const getProcessedFullname = (string, separator = " ") => {
-  const arrReverseFullName = string
-    .split(separator)
-    .reverse()
-    .map((elem) => elem.at(0).toUpperCase() + elem.slice(1));
-
-  return (
-    arrReverseFullName.shift() + " " + arrReverseFullName.map((elem) => elem.at(0) + ".").join()
-  );
-};
-
-console.log(getProcessedFullname(fullName));
