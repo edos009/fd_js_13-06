59cdb1383565d3eebf2f00b68123226a0fc42585
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-23 16:04:37 +0300

    Destructuring

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 "use strict";
 
-const numbers1 = [1,2,3,4,5,2,1];
-const numbers2 = [5,7,2,1];
+//Деструктуризация - способ создания переменных
 
-// const mySetArr = new Set(numbers1.concat(numbers2));
-const mySetArr = new Set([...numbers1, ...numbers2]);
+const user = {
+  id: 1,
+  privateInfo: {
+    fname: "Eduard",
+    sname: "Tiutiunnyk",
+    bday: {
+      day: 27,
+      month: 5,
+      year: 1996,
+    },
+    children: ["q1", "q2", "q3"],
+  },
+  contactInfo: {
+    phone: {
+      work: "123-12-95",
+      mobile: "007-007-007",
+    },
+    adress: {
+      town: "ZP",
+      street: "Gogola",
+      house: 70,
+    },
+    mail: "eduardtiutiunnyk@gmail.com",
+  },
+  professional: "director",
+  date: {
+    day: 2,
+    month: 9,
+    year: 2016,
+  },
+};
 
-console.log([...mySetArr]);
+// const prof = user.professional;
+const { professional } = user;
+console.log(professional);
+
+// const sname = user.privateInfo.sname;
+const {
+  privateInfo: { sname, fname},
+} = user;
+console.log(sname, fname);
+
+const {
+  privateInfo: {bday:{day:bDay,month:bMonth, year:bYear}},
+} = user;
+console.log(bDay, bMonth, bYear);
+
+const {
+  date: {day:goDay,month:goMonth, year:goYear},
+} = user;
+
+console.log(goDay, goMonth, goYear);
+
+const dayBDay = user.privateInfo.bday.day;
+const monthBDay = user.privateInfo.bday.month;
