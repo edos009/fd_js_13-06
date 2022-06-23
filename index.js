3ae17a9e996410e7baf40b1e6d322f879d39b433
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-23 15:36:08 +0300

    Example Set how type data

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
 "use strict";
 
-const dictionary = new Map();
+const numbers1 = [1,2,3,4,5,2,1];
+const numbers2 = [5,7,2,1];
 
-dictionary.set("мир", "peace");
-dictionary.set("небо", "sky");
-dictionary.set("людина", "man");
-dictionary.set("море", "sea");
-dictionary.set("поїхати", "go");
-dictionary.set("на", "on");
-dictionary.set("як", "as");
-dictionary.set("група", "group");
-dictionary.set("навчання", "teaching");
-dictionary.set("мова", "language");
-dictionary.set("програмування", "programming");
-dictionary.set("займатися", "engage in");
+// const mySetArr = new Set(numbers1.concat(numbers2));
+const mySetArr = new Set([...numbers1, ...numbers2]);
 
-const str = "Група студентів поїхати займатися Програмування на Море як мир";
-
-const translate = (str) => {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((word) => (dictionary.has(word) ? dictionary.get(word) : word))
-    .join(" ");
-};
-
-// console.log(translate(str));
-
-for (const iterator of dictionary.keys()) {
-  console.log(iterator);
-}
-// console.log(...dictionary.keys());
-
-for (const value of dictionary.values()) {
-  console.log(value);
-}
-// console.log(...dictionary.values());
-
-const arrayKeysMap = [...dictionary.keys()];
-console.log(arrayKeysMap);
+console.log([...mySetArr]);
